976ad1f574bf9b774180572d034feb298533fed3
kondr0 <kondr0@users.noreply.github.com>
2026-06-01 12:44:48 +1000

    Add .gitignore, remove build artifacts

--- todo-service/build/resources/main/db/migration/V2__create_tags_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE todo.tags (
-    id UUID PRIMARY KEY,
-    name VARCHAR(64) NOT NULL,
-    user_id UUID NOT NULL,
-    color VARCHAR(16)
-);
-
-CREATE INDEX idx_tags_user_id ON todo.tags(user_id);
-CREATE UNIQUE INDEX idx_tags_name_user ON todo.tags(name, user_id);
